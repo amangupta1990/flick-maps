85ab36d7d97f4075af2c022f71b750f1e41da41e
Aman Gupta <Aman Gupta>
2017-02-07 01:49:27 +0530

    testing webkit transforms

--- src/pages/animations.ts
+++ src/pages/animations.ts
@@ -7,12 +7,13 @@ export class Animations {
       state('inactive', style({
         opacity:0,
         transform: 'translateY(-100%)',
-        '-webkit-transform': 'translateY(-100%)'
+       webkitTransform: 'translateY(-100%)'
+        
       })),
       state('active',   style({
         opacity:1,
         transform: 'translateY(0%)',
-        '-webkit-transform': 'translateY(0%)'
+       webkitTransform: 'translateY(0%)'
       })),
       transition('inactive => active', animate('500ms ease-in')),
       transition('active => inactive', animate('500ms ease-out'))
@@ -21,12 +22,12 @@ export class Animations {
       state('inactive', style({
         opacity:0,
         transform: 'translateY(100%)',
-        '-webkit-transform': 'translateY(100%)'
+       webkitTransform: 'translateY(100%)'
       })),
       state('active',   style({
         opacity:1,
         transform: 'translateY(0%)',
-        '-webkit-transform': 'translateY(0%)'
+       webkitTransform: 'translateY(0%)'
       })),
       transition('inactive => active', animate('500ms ease-in')),
       transition('active => inactive', animate('500ms ease-out'))
